fffb309c7de60c3b55ee2a3656703b9ed32f7489
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2024-06-27 16:07:11 +0900

    Title: 특정 조건 두 정수 비교, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240627/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
+++ 240627/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -3,12 +3,12 @@ arr = inp.split()
 a = int(arr[0])
 b = int(arr[1])
 
-if a<b:
-    print(1, end=' ')
+if a < b:
+	print("1", end=" ")
 else:
-    print(0, end=' ')
+	print("0", end=" ")
 
-if a==b:
-    print(1)
+if a == b:
+	print("1")
 else:
-    print(0)
+	print("0")
