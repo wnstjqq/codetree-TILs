2c73e374e9a7784f5a2b370dca09afd04cb5c3fd
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2024-06-29 12:11:30 +0900

    Title: 두 사람, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240629/두 사람/two-person.py
+++ 240629/두 사람/two-person.py
@@ -1,7 +1,4 @@
 a = input().split()
 b = input().split()
 
-if (int(a[0]) >= 19 and a[1] == "M") or (int(b[1]) >= 19 and b[1] == "M"):
-    print(1)
-else:
-    print(0)
+print(int((int(a[0]) >= 19 and a[1] == "M") or (int(b[0]) >= 19 and b[1] == "M")))
