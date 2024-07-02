1a1fbf9bce3aa434b54be9c48179227c5d4b13af
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2024-07-02 21:42:37 +0900

    Title: 코로나 메뉴얼, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240702/코로나 메뉴얼/covid-manual.py
+++ 240702/코로나 메뉴얼/covid-manual.py
@@ -5,7 +5,8 @@ p3 = input().split()
 if p1[0] == "Y" and int(p1[1]) >= 37 or p2[0] == "Y" and int(p1[1]) >= 37 or p3[0] == "Y" and int(p1[1]) >= 37:
     if ((p1[0] == "Y" and int(p1[1]) >= 37) and (p2[0] == "Y" and int(p2[1]) >= 37)
     or (p1[0] == "Y" and int(p1[1])) and (p3[0] == "Y" and int(p3[1]) >= 37) 
-    or (p2[0] == "Y" and int(p2[1]) >= 37) and (p3[0] == "Y" and int(p3[1]) >= 37)):
+    or (p2[0] == "Y" and int(p2[1]) >= 37) and (p3[0] == "Y" and int(p3[1]) >= 37)
+    or (p1[0] == "Y" and int(p1[1]) >= 37) and (p2[0] == "Y" and int(p2[1]) >= 37) and (p3[0] == "Y" and int(p3[1]) >= 37)):
         print("E")
     else:
         print("N")
