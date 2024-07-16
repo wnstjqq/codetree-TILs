7c8aed18b186c1395b1f39fa678186074b387fc3
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2024-07-16 23:23:42 +0900

    Title: ab 사이에 없는 c, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240716/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -0,0 +1,11 @@
+arr = input().split()
+a,b,c = int(arr[0]), int(arr[1]), int(arr[2])
+
+satisfied = True
+for i in range(a,b+1):
+    if i % c == 0:
+        satisfied = False
+if satisfied == True:
+    print("True")
+else:
+    print("False")
