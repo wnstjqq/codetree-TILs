5974df084da8acff58232574732d3802e5365f2f
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2024-07-18 17:01:44 +0900

    Title: 체크 다이아몬드, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240718/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+for i in range(n):
+    for _ in range(n-i-1):
+        print(" ", end="")
+    for _ in range(i+1):
+        print("*", end=" ")
+    print()
+for j in range(n-1):
+    for _ in range(j+1):
+        print(" ", end="")
+    for _ in range(n-j-1):
+        print("*", end=" ")
+    print()
