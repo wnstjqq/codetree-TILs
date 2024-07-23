275dae17a29b5719188c4760c81265b55a0bf6c3
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2024-07-23 17:35:26 +0900

    Title: 행에 따라 다르게 출력, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240723/행에 따라 다르게 출력/output-differently-for-row.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(n):
+        if i % 2 == 0:
+            if i == 0:
+                print(j+1, end=" ")
+            else:
+                print(3 * n + j+1, end=" ")
+        else:
+            if i == 1:
+                print(n + 2 * (j+1), end=" ")
+            else:
+                print(3 * n + n + 2 * (j+1))
+    print()
