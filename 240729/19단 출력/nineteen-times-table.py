caba2e9626674883f9050281975aaad67213c419
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2024-07-29 17:55:22 +0900

    Title: 19단 출력, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240729/19단 출력/nineteen-times-table.py
@@ -0,0 +1,12 @@
+for i in range(1, 20):
+    for j in range(1, 20):
+        if i % 2 == 1:
+            if (i * j) % 2 == 1 and j < 19:
+                print(f"{i} * {j} = {i*j}", end=" / ")
+            else:
+                print(f"{i} * {j} = {i*j}", end="\n")
+        else:
+            if j % 2 == 1 and j < 19:
+                print(f"{i} * {j} = {i*j}", end=" / ")
+            else:
+                print(f"{i} * {j} = {i*j}", end="\n")
