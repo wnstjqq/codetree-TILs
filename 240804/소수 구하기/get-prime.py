07becf69eaf01cb258b42c9c228986d678421e1f
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2024-08-04 22:39:17 +0900

    Title: 소수 구하기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240804/소수 구하기/get-prime.py
+++ 240804/소수 구하기/get-prime.py
@@ -3,9 +3,9 @@ n = int(input())
 for i in range(2, n+1):
     is_prime = True
 
-    for j in range(2, int(n**0.5) + 1):
+    for j in range(2, int(i**0.5) + 1):
         if i % j == 0:
             is_prime = False
-            
+
     if is_prime == True:
         print(i, end=" ")
