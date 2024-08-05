15398aa9bdc598a4d213595d24aa8e738a532f2e
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2024-08-06 01:57:14 +0900

    Title: 배열의 특정 위치 찾기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240806/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ 240806/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -4,13 +4,11 @@ s_even = 0
 s_odd = 0
 cnt_odd = 0
 
-for elem in arr:
-    if elem % 2 == 0:
-        s_even += elem
-
-for i in range(1, len(arr) + 1):
-    if i % 3 == 0:
-        s_odd += arr[i-1]
+for i in range(len(arr)):
+    if (i+1) % 2 == 0:
+        s_even += arr[i]
+    elif (i+1) % 3 == 0:
+        s_odd += arr[i]
         cnt_odd += 1
 
 print(f"{s_even} {s_odd / cnt_odd:.1f}")
