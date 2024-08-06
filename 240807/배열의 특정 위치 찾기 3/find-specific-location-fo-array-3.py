09f4aeb70a0ca82f9d0b67cedffe23e92c26a000
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2024-08-07 01:02:45 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240807/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
+++ 240807/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
@@ -4,5 +4,6 @@ for i in range(len(arr)):
     if arr[i] == 0:
         print(arr[i-1] + arr[i-2] + arr[i-3])
         break
-
-print(arr[-1] + arr[-2] + arr[-3])
+    else:
+        print(arr[-1] + arr[-2] + arr[-3])
+        break
