6de0ea16df869dc7981c62fe105465272a2b01d6
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2024-08-10 22:00:43 +0900

    Title: 연속부분수열일까, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240810/연속부분수열일까/contiguous-array-or-not.py
+++ 240810/연속부분수열일까/contiguous-array-or-not.py
@@ -7,17 +7,15 @@ if arr2[0] not in arr1:
     print("No")
 
 for _ in range(arr1.count(arr2[0])):
-    main_list, sub_list = [], []
+    main_list = []
     
     idx = arr1.index(arr2[0])
     
-    for i in range(len(arr2)):
+    while idx != len(arr1):
         main_list.append(arr1[idx])
-        sub_list.append(arr2[i])
-        
         idx += 1
 
-    if main_list == sub_list:
+    if main_list == arr2:
         print("Yes")
         break
     else:
