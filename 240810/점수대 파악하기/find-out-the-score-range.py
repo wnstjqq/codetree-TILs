0310203d979625afa5599995a760c0251ad10d2d
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2024-08-10 16:37:44 +0900

    Title: 점수대 파악하기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240810/점수대 파악하기/find-out-the-score-range.py
+++ 240810/점수대 파악하기/find-out-the-score-range.py
@@ -6,7 +6,8 @@ grade = [x * 10 for x in range(10, 0, -1)]
 for elem in arr:
     if elem == 0:
         break
-    cnt[elem // 10 - 1] += 1
+    if elem >= 10:
+        cnt[elem // 10 - 1] += 1
 
 for i in range(10):
     print(grade[i], "-", cnt[9-i])
