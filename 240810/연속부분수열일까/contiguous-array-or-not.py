c06ced56581d69167ce7de0f3b5e609a4c8cca45
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2024-08-10 21:04:04 +0900

    Title: 연속부분수열일까, Time: 5020ms, Memory: 288MB, Status: Time Limit Exceed - CodeTree

--- 240810/연속부분수열일까/contiguous-array-or-not.py
+++ 240810/연속부분수열일까/contiguous-array-or-not.py
@@ -7,13 +7,14 @@ for elem1 in arr1:
     if elem1 == arr2[0]:
         idx = arr1.index(arr2[0])
 
-while idx < len(arr2):
+while True:
+    if arr1[idx] == arr2[-1]:
+        print("Yes")
+        break
+        
     for elem2 in arr2:
         if elem2 != arr1[idx]:
             print("No")
             break
         else:
             idx += 1
-    
-if idx == arr2.index(arr2[-1]):
-    print("Yes")
