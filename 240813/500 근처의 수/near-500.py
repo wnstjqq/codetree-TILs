3dd539380e7e0b686b691fec9c71b95157cd3f03
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2024-08-13 21:43:07 +0900

    Title: 500 근처의 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240813/500 근처의 수/near-500.py
@@ -0,0 +1,11 @@
+arr = list(map(int, input().split()))
+
+over500, under500 = [], []
+
+for elem in arr:
+    if elem > 500:
+        over500.append(elem)
+    else:
+        under500.append(elem)
+
+print(max(under500), min(over(500)))
