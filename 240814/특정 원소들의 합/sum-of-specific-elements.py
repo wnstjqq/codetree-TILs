70ff9f4d7a50fda7ca0c18b9628f2b83e3640861
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2024-08-14 21:01:45 +0900

    Title: 특정 원소들의 합, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240814/특정 원소들의 합/sum-of-specific-elements.py
@@ -0,0 +1,12 @@
+arr_2d = [
+    list(map(int, input().split()))
+    for _ in range(4)
+]
+
+sum_val = 0
+for i in range(4):
+    for j in range(4):
+        if i >= j:
+            sum_val += arr_2d[i][j]
+
+print(sum_val)
