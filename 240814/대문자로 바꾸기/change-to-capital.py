50598c938ce71d93454f105d2418f6369fe88014
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2024-08-14 18:42:19 +0900

    Title: 대문자로 바꾸기, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240814/대문자로 바꾸기/change-to-capital.py
+++ 240814/대문자로 바꾸기/change-to-capital.py
@@ -1,8 +1,7 @@
-upper = "A B C D E F G H I J K L M N O".split()
-cnt = 0
+lower = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split()
+upper = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".split()
 for _ in range(5):
     arr = input().split()
-    for _ in range(len(arr)):
-        print(upper[cnt], end=" ")
-        cnt += 1
+    for i in range(3):
+        print(upper[lower.index(arr[i])], end=" ")
     print()
