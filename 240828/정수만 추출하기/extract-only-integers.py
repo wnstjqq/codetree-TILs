b66a1e6a6abfd12998ddce6e21ffbf6c133c9f5a
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2024-08-28 15:11:50 +0900

    Title: 정수만 추출하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240828/정수만 추출하기/extract-only-integers.py
@@ -0,0 +1,21 @@
+A, B = input().split()
+
+s = 0
+n1 = ""
+n2 = ""
+
+for elem1 in A:
+    if elem1.isdigit():
+        n1 += elem1
+    else:
+        s += int(n1)
+        n1 = ""
+    
+for elem2 in B:   
+    if elem2.isdigit():
+        n2 += elem2
+    else:
+        s += int(n2)
+        n2 = ""
+
+print(s)
