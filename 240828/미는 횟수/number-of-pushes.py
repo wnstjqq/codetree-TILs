556e3b382171234474148ad4813dfc68e845c90e
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2024-08-28 16:01:51 +0900

    Title: 미는 횟수, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240828/미는 횟수/number-of-pushes.py
@@ -0,0 +1,15 @@
+A = input()
+B = input()
+
+cnt = 0
+
+for _ in range(len(A) + 1):
+    if A == B: break
+
+    A = A[-1] + A[:-1]
+    cnt += 1
+
+if cnt == 7:
+    print(-1)
+else:
+    print(cnt)
