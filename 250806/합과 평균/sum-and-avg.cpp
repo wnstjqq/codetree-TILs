e6e82baea008d4564fc02f016a305ea8b2bda7ce
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2025-08-06 16:26:40 +0900

    Title: 합과 평균, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250806/합과 평균/sum-and-avg.cpp
+++ 250806/합과 평균/sum-and-avg.cpp
@@ -5,14 +5,14 @@ int main() {
     int A, B;
     cin >> A >> B;
 
-    cout << A + B << "\n";
+    cout << A + B << " ";
 
-    double avg = (A + B) / 2;
+    double avg = (A + B) / 2.0;
 
     cout << fixed;
     cout.precision(1);
 
     cout << avg;
-     
+
     return 0;
 }
