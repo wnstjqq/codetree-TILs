eed326f28804fdc8248c005c47e1eab5c2e80400
wnstjqq <168539001+wnstjqq@users.noreply.github.com>
2026-03-08 12:44:37 +0900

    Title: 절댓값으로 변경, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260308/절댓값으로 변경/find-the-absolute-value.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define MAX_N 50
+
+int arr[MAX_N];
+
+void absoluteValue(int* arr, int n) {
+    for (int i = 0; i < n; i++) {
+        if (*(arr + i) < 0) {
+            *(arr + i) * -1;
+        }
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    absoluteValue(arr, n);
+
+    for (int i = 0; i < n; i++) {
+        printf(arr[i]);
+    }
+    
+    return 0;
+}
